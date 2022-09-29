1419fb2204d56c784eac529ab8d2c93986eefaa5
basile <basile-lequeux@hotmail.fr>
2022-09-29 16:05:23 +0200

    jsp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import Grid from "./Grid";
+import {useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const [cursor, setCursor] = useState(0);
+
+    return (
+        <div className="App">
+            <Grid
+                cursor={cursor}
+                isPlaying={isPlaying}
+            />
+
+            <button className="button_play"
+                    onClick={() => setIsPlaying(!isPlaying)}>{isPlaying ? "STOP" : "PLAY"}
+            </button>
+            <button className="button_play"
+                    onClick={() => setCursor(cursor+1)}> +
+            </button>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Grid.js
@@ -0,0 +1,11 @@
+import React, {useEffect, useState} from 'react'
+import './grid.css'
+
+export default function Grid({cursor, isPlaying}) {
+
+
+    return (
+        <div className="grid-container">
+        </div>
+    )
+}
