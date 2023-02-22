823db7d5d1ef38a5432bf2fd210b81385f471502
Marwen <marwen.jaiem@gmail.com>
2023-02-22 23:20:31 +0100

    Fix timeline 🚑️ 🚑️ 🚑️

--- src/components/CursorStart.js
+++ src/components/CursorStart.js
@@ -1,87 +1,62 @@
-import {Flex, Text} from "@chakra-ui/react";
-import {usePlayerContext} from "../PlayerContext";
+import { Flex, Text } from "@chakra-ui/react";
+import { usePlayerContext } from "../PlayerContext";
 import "../App.css";
 import React from "react";
 
 export default function CursorStart() {
-    const {
-        nbrOfBeat,
-        handleCursorStart,
-        cursor,
-        cursorStartingPoint,
-        setIsSelectStartPoint,
-        isSelectStartPoint,
-        cursorEndingPoint
-    } = usePlayerContext();
+  const {
+    nbrOfBeat,
+    handleCursorStart,
+    cursor,
+    cursorStartingPoint,
+    setIsSelectStartPoint,
+    isSelectStartPoint,
+    cursorEndingPoint,
+  } = usePlayerContext();
 
-    const getStyleDotTimeline = (index, isSpacer) => {
-        if (isSpacer) {
-            return cursor === index ? "timeline_dot_spacer_active" : "timeline_dot_spacer";
-        } else {
-            return cursor === index ? "timeline_dot_active" : "timeline_dot";
-        }
-
+  const getStyleDotTimeline = (index, isSpacer) => {
+    if (isSpacer) {
+      return cursor === index
+        ? "timeline_dot_spacer_active"
+        : "timeline_dot_spacer";
+    } else {
+      return cursor === index ? "timeline_dot_active" : "timeline_dot";
     }
+  };
 
-    const isStartingPoint = (index) => {
-        if (cursorStartingPoint === index || cursorEndingPoint === (index + 1)) {
-            return {color: '#61dafb', border: '1px solid #61dafb'}
-        }
+  const isStartingPoint = (index) => {
+    if (cursorStartingPoint === index || cursorEndingPoint === index + 1) {
+      return { color: "#61dafb", border: "1px solid #61dafb" };
     }
+  };
 
-    return (
-        <Flex>
-            <Flex bg="#242424" alignItems={"center"} h={"30px"} w="180px">
-                <Text
-                    color={"white"}
-                    h={"15px"}
-                    w={"100px"}
-                    style={{marginLeft: "30px", marginRight: "15px"}}
-                >
-                    Timeline
-                </Text>
-                {isSelectStartPoint &&
-                    <Text
-                        color={"white"}
-                        h={'14px'}
-                        onClick={() => setIsSelectStartPoint(false)}
-                    >
-                        START
-                    </Text>
-                }
-                {!isSelectStartPoint &&
-                    <Text
-                        color={"white"}
-                        h={'14px'}
-                        onClick={() => setIsSelectStartPoint(true)}
-                    >
-                        END
-                    </Text>
-                }
-            </Flex>
-            <Flex>
-                {[...Array(nbrOfBeat)].map((x, index) =>
-                    (index + 1) % 4 === 0 ? (
-                        <div
-                            key={index}
-                            className={getStyleDotTimeline(index, true)}
-                            onClick={() => handleCursorStart(index)}
-                            style={isStartingPoint(index)}
-                        >
-                            {index + 1}
-                        </div>
-                    ) : (
-                        <div
-                            key={index}
-                            className={getStyleDotTimeline(index, false)}
-                            onClick={() => handleCursorStart(index)}
-                            style={isStartingPoint(index)}
-                        >
-                            {index + 1}
-                        </div>
-                    )
-                )}
-            </Flex>
-        </Flex>
-    );
+  return (
+    <Flex>
+      <Flex bg="#242424" alignItems={"center"} h={"30px"} w="180px">
+        <Text color={"white"} h={"15px"} w={"130px"}>
+          Timeline
+        </Text>
+        <Text
+          color={"white"}
+          h={"14px"}
+          w={"50px"}
+          onClick={() => setIsSelectStartPoint(!isSelectStartPoint)}
+        >
+          {isSelectStartPoint ? "START" : "END"}
+        </Text>
+      </Flex>
+      <Flex>
+        {[...Array(nbrOfBeat)].map((x, index) => (
+          <div
+            key={index}
+            className={getStyleDotTimeline(index, (index + 1) % 4 === 0)}
+            onClick={() => handleCursorStart(index)}
+            style={isStartingPoint(index)}
+          >
+            {index + 1}
+          </div>
+        ))}
+      </Flex>
+    </Flex>
+  );
 }
--- src/components/SideBarSelect.js
+++ src/components/SideBarSelect.js
@@ -1,83 +1,80 @@
-import React, {useEffect, useState} from "react";
-import {drumKitList, instrumentKit} from "../PlaySound";
-import {Box, Flex, Select} from "@chakra-ui/react";
-import {FaMinus, FaPlus} from "react-icons/fa";
+import React, { useEffect, useState } from "react";
+import { drumKitList, instrumentKit } from "../PlaySound";
+import { Box, Flex, Select } from "@chakra-ui/react";
+import { FaMinus, FaPlus } from "react-icons/fa";
 import "../App.css";
-import {usePlayerContext} from "../PlayerContext";
+import { usePlayerContext } from "../PlayerContext";
 
 const SideBarSelect = ({
-    trackId,
-    instrument,
-    type = "rhythm",
-    index,
-    length,
+  trackId,
+  instrument,
+  type = "rhythm",
+  index,
+  length,
 }) => {
-    const {handleChangeInstrument, addRhythmTrack, deleteOneTrack, nbrOfTrack} =
-        usePlayerContext();
+  const { handleChangeInstrument, addRhythmTrack, deleteOneTrack, nbrOfTrack } =
+    usePlayerContext();
 
-    const [canAddTrack, setCanAddTrack] = useState(true);
+  const [canAddTrack, setCanAddTrack] = useState(true);
 
-    useEffect(() => {
-        if (nbrOfTrack > 7) {
-            setCanAddTrack(false);
-        } else {
-            setCanAddTrack(true);
-        }
-    }, [nbrOfTrack]);
+  useEffect(() => {
+    if (nbrOfTrack > 7) {
+      setCanAddTrack(false);
+    } else {
+      setCanAddTrack(true);
+    }
+  }, [nbrOfTrack]);
 
-    return (
-        <Flex
-            h={"50px"}
-            justify={"center"}
-            alignItems={"center"}
-            style={{marginRight: "20px", marginLeft: "23px"}}
+  return (
+    <Flex h={"50px"} justify={"center"} alignItems={"center"}>
+      <Box
+        paddingLeft={"20px"}
+        paddingRight={"20px"}
+        minW={"130px"}
+        w={"130px"}
+      >
+        <Select
+          bg={"white"}
+          h={"30px"}
+          onChange={(e) => handleChangeInstrument(trackId, e.target.value)}
+          value={instrument}
+          size={"xs"}
         >
-            <Box>
-                <Select
-                    bg={"white"}
-                    h={"30px"}
-                    onChange={(e) => handleChangeInstrument(type, trackId, e.target.value)}
-                    value={instrument}
-                    size={"xs"}
-                >
-                    {type === "rhythm" &&
-                        drumKitList.map((elem, i) => (
-                            <option key={i} value={elem.label}>
-                                {elem.label}
-                            </option>
-                        ))}
-                    {type === "melodic" &&
-                        instrumentKit.map((elem, i) => (
-                            <option key={i} value={elem.value}>
-                                {elem.label}
-                            </option>
-                        ))}
-                </Select>
-            </Box>
-            {length >= 2 ? (
-                <button
-                    className="button_delete_track"
-                    onClick={() => deleteOneTrack(index, type)}
-                    style={{marginLeft: "5px"}}
-                >
-                    <FaMinus size={"20"}/>
-                </button>
-            ) : (
-                <div style={{paddingRight: "25px"}}/>
-            )}
-            {index === length - 1 && canAddTrack && type !== "melodic" ? (
-                <button
-                    className="button_add_track"
-                    onClick={() => addRhythmTrack()}
-                    style={{marginLeft: "7px"}}
-                >
-                    <FaPlus size={"20"}/>
-                </button>
-            ) : (
-                <div style={{paddingRight: "25px"}}/>
-            )}
-        </Flex>
-    );
+          {type === "rhythm" &&
+            drumKitList.map((elem, i) => (
+              <option key={i} value={elem.label}>
+                {elem.label}
+              </option>
+            ))}
+          {type === "melodic" && <option value="synth">synth</option>}
+        </Select>
+      </Box>
+      <Flex w={"50px"} justifyContent={"center"}>
+        {length >= 2 ? (
+          <button
+            className="button_delete_track"
+            onClick={() => deleteOneTrack(index, type)}
+            style={{ marginLeft: "5px" }}
+          >
+            <FaMinus size={"20"} />
+          </button>
+        ) : (
+          <div style={{ paddingRight: "25px" }} />
+        )}
+        {index === length - 1 && canAddTrack && type !== "melodic" ? (
+          <button
+            className="button_add_track"
+            onClick={() => addRhythmTrack()}
+            style={{ marginLeft: "7px" }}
+          >
+            <FaPlus size={"20"} />
+          </button>
+        ) : (
+          <div style={{ paddingRight: "25px" }} />
+        )}
+      </Flex>
+    </Flex>
+  );
 };
 
 export default SideBarSelect;
